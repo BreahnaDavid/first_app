ee66b13154586501b769b0be49f6c2b0d34ee518
BreahnaDavid <breahnadavid@gmail.com>
2016-09-07 23:56:42 +0300

    Remember Token to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  def new_remember_token
+  def self.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
-  def encrypt(token)
+  def self.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
 
   private
 
   def create_remember_token
-    self.remember_token = encrypt(new_remember_token)
+    self.remember_token = User.encrypt(User.new_remember_token)
   end
 end
